fb6c6ae7c95f54d05c6d8f1e9b168111101c73bf
Vinselcia <153624107+Vinselcia@users.noreply.github.com>
2024-02-15 12:17:48 +0530

    Final Commit

---
+++ WelcomeApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelcomeApp;
+Console.WriteLine("Welcome to Authentication app");
+
+SignIn obj = new SignIn();
+
+Console.WriteLine(obj.Authenticate("admin", "admin@123"));
